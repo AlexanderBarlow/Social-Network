ccc8a8eb75a38e330183ecf06213d0e349f3be02
alex barlow <alex.barlow@rocketmail.com>
2023-01-16 14:42:36 -0500

    added notes for userController of what needs to be done

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,18 @@
 const { User, Thought } = require('../models');
 
 module.exports = {
+//getUser route which finds a user
+
+//getSingleUser route which finds a single user
+
+//createUser route which creates a single user
+
+//updateUser route which updates a user
+
+//deleteUser route which deletes a user
+
+//addFriend route which adds a friend to a user
+
+//deleteFriend which deletes a friend per user
 
 };
