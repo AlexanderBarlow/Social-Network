fe1e6d4d47d300d279d6c36a7ed3cd31a571d90c
alex barlow <alex.barlow@rocketmail.com>
2023-01-16 14:44:38 -0500

    added required functions in user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,17 +2,31 @@ const { User, Thought } = require('../models');
 
 module.exports = {
 //getUser route which finds a user
+getUser(req, res) {
 
+},
 //getSingleUser route which finds a single user
+getSingleUser(req, res) {
 
+},
 //createUser route which creates a single user
+createUser(req, res) {
 
+},
 //updateUser route which updates a user
+updateUser(req, res) {
 
+},
 //deleteUser route which deletes a user
+deleteUser(req, res) {
 
+},
 //addFriend route which adds a friend to a user
+addFriend(req, res) {
 
+},
 //deleteFriend which deletes a friend per user
+deleteFriend(req, res) {
 
+},
 };
