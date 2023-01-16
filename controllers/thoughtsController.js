f5ac5e1a1a6f09b930186c826d0d953288fe5240
alex barlow <alex.barlow@rocketmail.com>
2023-01-16 16:42:07 -0500

    added notes for todos on thoughts controller

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -1,5 +1,18 @@
 const { User, Thought } = require('../models');
 
 module.exports = {
+//add getThought route
+
+//add getSingleThought route
+
+//add createThough route
+
+//add updateThought route
+
+//add deleteThough route
+
+//add createReaction route
+
+//add deleteReaction route
 
 };
