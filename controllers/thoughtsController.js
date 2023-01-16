8db5362b2721cfef694a17edc6520b15a9725103
alex barlow <alex.barlow@rocketmail.com>
2023-01-16 16:44:22 -0500

    added the functions needed for thought routes

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -2,17 +2,31 @@ const { User, Thought } = require('../models');
 
 module.exports = {
 //add getThought route
+getThough(req, res) {
 
+},
 //add getSingleThought route
+getSingleThought(req, res) {
 
-//add createThough route
+},
+//add createThought route
+createThought(req, res) {
 
+},
 //add updateThought route
+updateThought(req, res) {
 
+},
 //add deleteThough route
+deletThought(req, res) {
 
+},
 //add createReaction route
+createReaction(req, res) {
 
+},
 //add deleteReaction route
+deleteReaction(req, res) {
 
+},
 };
