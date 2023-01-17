328e70725667e8e63c8ae0e761ec944305cbe312
alex barlow <alex.barlow@rocketmail.com>
2023-01-17 10:39:57 -0500

    added route for creating a reaction

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -60,7 +60,17 @@ deleteThought(req, res) {
 },
 //add createReaction route
 createReaction(req, res) {
-
+    Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $addToSet: { reactions: req.body } },
+        { runValidators: true, new: true }
+    )
+    .then((thought) => 
+        !thought
+            ? res.status(404).json({ message: 'No thought found with that Id.' })
+            :res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
 },
 //add deleteReaction route
 deleteReaction(req, res) {
