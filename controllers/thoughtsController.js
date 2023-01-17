1cde79fa48e84def848c20225db5d4b7b14b5579
alex barlow <alex.barlow@rocketmail.com>
2023-01-17 10:13:34 -0500

    made get route for thoughts

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -2,8 +2,10 @@ const { User, Thought } = require('../models');
 
 module.exports = {
 //add getThought route
-getThough(req, res) {
-
+getThought(req, res) {
+Thought.find({})
+    .then((thought) => res.json(thought))
+    .catch((err) => res.status(500).json(err));
 },
 //add getSingleThought route
 getSingleThought(req, res) {
